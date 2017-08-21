ea9419ee3b8753c24e16f827855f3656d0ffac29
dennissheppard <dennis.sheppard@gmail.com>
2017-08-20 22:29:38 -0500

    sw-precache, sw-toolbox, build process tweaks

---
+++ sw-precache-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  navigateFallback: '/index.html',
+  stripPrefix: 'dist',
+  root: 'dist/',
+  staticFileGlobs: [
+    'dist/index.html',
+    'dist/**.js',
+    'dist/**.css'
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: '/*',
+      handler: 'cacheFirst',
+      options: {
+        origin: 'tmdb.org',
+        cache: {
+          maxEntries: 100,
+          name: 'movie-cache'
+        }
+      }
+    },
+    {
+      urlPattern: '/*',
+      handler: 'cacheFirst',
+      options: {
+        origin: 'themoviedb.org',
+        cache: {
+          maxEntries: 10,
+          name: 'movie-cache'
+        }
+      }
+    }
+  ]
+};
